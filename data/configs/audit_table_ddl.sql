2442700e11cc89178c7b68b8d21f0f0aae1d9259
mahikilari <133624942+mahikilari@users.noreply.github.com>
2025-07-30 00:14:46 +0530

    Delete data directory

--- data/configs/audit_table_ddl.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE gcp-practice-1-464706.temp_dataset.audit_log (
-    data_source STRING,
-    tablename STRING,
-    load_type STRING,
-    record_count INT64,
-    load_timestamp TIMESTAMP,
-    status STRING
-);
