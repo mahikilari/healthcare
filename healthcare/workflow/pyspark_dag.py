2715f51792449117073a9c426c9e91a3e7b26ca9
mahikilari <133624942+mahikilari@users.noreply.github.com>
2025-07-29 12:32:40 +0530

    Update pyspark_dag.py

--- healthcare/workflow/pyspark_dag.py
+++ healthcare/workflow/pyspark_dag.py
@@ -13,7 +13,7 @@ from airflow.providers.google.cloud.operators.dataproc import (
 PROJECT_ID = "gcp-practice-1-464706"
 REGION = "us-east1"
 CLUSTER_NAME = "my-demo-cluster2"
-COMPOSER_BUCKET = "us-central1-demo-instance-a649b781-bucket" ## change this 
+COMPOSER_BUCKET = us-south1-gcp-practice-1-46-5336bb08-bucket" ## change this 
 
 GCS_JOB_FILE_1 = f"gs://{COMPOSER_BUCKET}/data/INGESTION/hospitalA_mysqlToLanding.py"
 PYSPARK_JOB_1 = {
