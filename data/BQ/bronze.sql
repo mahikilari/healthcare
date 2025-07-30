62123cab085566e62a6ec3d31d38e889af5438b1
mahikilari <133624942+mahikilari@users.noreply.github.com>
2025-07-30 13:15:42 +0530

    Update bronze.sql

--- data/BQ/bronze.sql
+++ data/BQ/bronze.sql
@@ -4,31 +4,31 @@
 CREATE EXTERNAL TABLE IF NOT EXISTS `gcp-practice-1-464706.bronze_dataset.departments_ha` 
 OPTIONS (
   format = 'JSON',
-  uris = ['gs://healthcare-bucket-22032025/landing/hospital-a/departments/*.json']
+  uris = ['gs://bucket_202507/landing/hospital-a/departments/*.json']
 );
 
 CREATE EXTERNAL TABLE IF NOT EXISTS `gcp-practice-1-464706.bronze_dataset.encounters_ha` 
 OPTIONS (
   format = 'JSON',
-  uris = ['gs://healthcare-bucket-22032025/landing/hospital-a/encounters/*.json']
+  uris = ['gs://bucket_202507/landing/hospital-a/encounters/*.json']
 );
 
 CREATE EXTERNAL TABLE IF NOT EXISTS `gcp-practice-1-464706.bronze_dataset.patients_ha` 
 OPTIONS (
   format = 'JSON',
-  uris = ['gs://healthcare-bucket-22032025/landing/hospital-a/patients/*.json']
+  uris = ['gs://bucket_202507/landing/hospital-a/patients/*.json']
 );
 
 CREATE EXTERNAL TABLE IF NOT EXISTS `gcp-practice-1-464706.bronze_dataset.providers_ha` 
 OPTIONS (
   format = 'JSON',
-  uris = ['gs://healthcare-bucket-22032025/landing/hospital-a/providers/*.json']
+  uris = ['gs://bucket_202507/landing/hospital-a/providers/*.json']
 );
 
 CREATE EXTERNAL TABLE IF NOT EXISTS `gcp-practice-1-464706.bronze_dataset.transactions_ha` 
 OPTIONS (
   format = 'JSON',
-  uris = ['gs://healthcare-bucket-22032025/landing/hospital-a/transactions/*.json']
+  uris = ['gs://bucket_202507/landing/hospital-a/transactions/*.json']
 );
 
 ---------------------------------------------------------------------------------------------------------------------------
@@ -36,31 +36,31 @@ OPTIONS (
 CREATE EXTERNAL TABLE IF NOT EXISTS `gcp-practice-1-464706.bronze_dataset.departments_hb` 
 OPTIONS (
   format = 'JSON',
-  uris = ['gs://healthcare-bucket-22032025/landing/hospital-b/departments/*.json']
+  uris = ['gs://bucket_202507/landing/hospital-b/departments/*.json']
 );
 
 CREATE EXTERNAL TABLE IF NOT EXISTS `gcp-practice-1-464706.bronze_dataset.encounters_hb` 
 OPTIONS (
   format = 'JSON',
-  uris = ['gs://healthcare-bucket-22032025/landing/hospital-b/encounters/*.json']
+  uris = ['gs://bucket_202507/landing/hospital-b/encounters/*.json']
 );
 
 CREATE EXTERNAL TABLE IF NOT EXISTS `gcp-practice-1-464706.bronze_dataset.patients_hb` 
 OPTIONS (
   format = 'JSON',
-  uris = ['gs://healthcare-bucket-22032025/landing/hospital-b/patients/*.json']
+  uris = ['gs://bucket_202507/landing/hospital-b/patients/*.json']
 );
 
 CREATE EXTERNAL TABLE IF NOT EXISTS `gcp-practice-1-464706.bronze_dataset.providers_hb` 
 OPTIONS (
   format = 'JSON',
-  uris = ['gs://healthcare-bucket-22032025/landing/hospital-b/providers/*.json']
+  uris = ['gs://bucket_202507/landing/hospital-b/providers/*.json']
 );
 
 CREATE EXTERNAL TABLE IF NOT EXISTS `gcp-practice-1-464706.bronze_dataset.transactions_hb` 
 OPTIONS (
   format = 'JSON',
-  uris = ['gs://healthcare-bucket-22032025/landing/hospital-b/transactions/*.json']
+  uris = ['gs://bucket_202507/landing/hospital-b/transactions/*.json']
 );
 
 ---------------------------------------------------------------------------------------------------------------------------
